628a4bd1fb69f12e25bcaf23d80c7a39c6159474
Howard Seltman <hseltman@cmu.edu>
2016-09-07 17:00:49 -0400

    Fix 'capIsNew='
    'capIsNew=TRUE' option did not lowercase the first character

--- camelCase.R
+++ camelCase.R
@@ -13,7 +13,11 @@
 camelCase = function(sv, upper=FALSE, capIsNew=FALSE, alreadyTrimmed=FALSE) {
   if (!is.character(sv)) stop("'sv' must be a string vector")
   if (!alreadyTrimmed) sv = gsub("[[:space:]]*$", "", gsub("^[[:space:]]*", "", sv))
-  if (capIsNew) sv = gsub("([A-Z])", " \\1", sv)
+  if (capIsNew)   if (capIsNew) {
+    sv = gsub("([A-Z])", " \\1", sv)
+    sv = gsub("^[[:space:]]", "", sv)
+    sv = tolower(sv)
+  }
   apart = strsplit(sv, split="[[:space:][:punct:]]")
   apart = lapply(apart, tolower)
   capitalize = function(x) paste0(toupper(substring(x,1,1)), substring(x,2))
